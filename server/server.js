ee7896dec25268e0e7d101f01a8fccc54b1504e0
Rodel Ruiz <Rodel Ruiz>
2016-12-20 12:12:03 +0300

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -115,12 +115,11 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-// POST /users/login {email, password}
+
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
   User.findByCredentials(body.email, body.password).then((user) => {
-    //res.send(user);
     return user.generateAuthToken().then((token) => {
       res.header('x-auth', token).send(user);
     })
